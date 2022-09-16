a54450c29cec22a77e6e1badbeaf0d33ab7ae5eb
Utsav Desai <20it026@charusat.edu.in>
2022-09-16 11:40:22 +0530

    15th commit

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -60,6 +60,7 @@ export default function StudentList(props) {
                                             <ButtonGroup>
                                                 <Link to={"/student/" + student.id}><Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button></Link>{' '}
                                                 <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this, student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                                                {/* <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this, student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button> */}
                                             </ButtonGroup>
                                         </td>
                                     </tr>
